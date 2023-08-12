b46456e6adbe17c8d7647c73424a926374e65e26
saneyoshi <aaa0410as@gmail.com>
2023-08-12 12:08:41 +0900

    add:react router, Login form add.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,20 @@
 import './App.css';
-import Menu from './_components/Menu';
-import Nav from './_components/Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Users from './pages/Users';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
 
-      <Nav />
-
-      <div className="container-fluid">
-        <div className="row">
-
-          <Menu />
-
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </main>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Users />} />
+          <Route path={'/login'} element={<Login />} />
+          <Route path={'/register'} element={<Register />} />
+        </Routes>
+      </BrowserRouter>
 
     </div>
   );
