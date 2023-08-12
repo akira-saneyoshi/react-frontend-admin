1563adad7b9c4edca6a0f119a3983920303a09b2
saneyoshi <aaa0410as@gmail.com>
2023-08-13 01:20:04 +0900

    add:Creating Products

---
+++ src/pages/products/ProductForm.tsx
@@ -0,0 +1,60 @@
+import { SyntheticEvent, useState } from 'react';
+import Layout from '../../_components/Layout';
+import { Button, TextField } from '@mui/material';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
+
+const ProductForm = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+  const [price, setPrice] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    await axios.post('products', {
+      title,
+      description,
+      image,
+      price
+    });
+
+    setRedirect(true);
+  }
+
+  if (redirect) {
+    return <Navigate to={'/products'}/>;
+  }
+
+  return (
+    <Layout>
+      <form onSubmit={submit}>
+        <div className="mb-3">
+          <TextField label="Title"
+            onChange={e => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <TextField label="Description" rows={4} multiline
+            onChange={e => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <TextField label="Image"
+            onChange={e => setImage(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <TextField label="Price" type="number"
+            onChange={e => setPrice(e.target.value)}
+          />
+        </div>
+        <Button variant="contained" color="primary" type="submit">Submit</Button>
+      </form>
+    </Layout>
+  );
+};
+
+export default ProductForm;
