20a365556ccc48d74889c8c750f277e493c16134
saneyoshi <aaa0410as@gmail.com>
2023-08-12 18:13:53 +0900

    add:axios add, Login post, Access-Control-Allow-Origin resolve.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,45 @@
+import React, { SyntheticEvent, useState } from 'react';
 import '../Login.css';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    await axios.post('login', {
+      email: email,
+      password: password
+    }, {withCredentials: true});
+
+    setRedirect(true);
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'}/>;
+  }
+
   return (
     <main className="form-signin">
-      <form>
+      <form onSubmit={submit}>
         <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
         <div className="form-floating">
-          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"
+            onChange={e => setEmail(e.target.value)} />
           <label htmlFor="floatingInput">Email address</label>
         </div>
         <div className="form-floating">
-          <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+          <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
+            onChange={e => setPassword(e.target.value)} />
           <label htmlFor="floatingPassword">Password</label>
         </div>
 
         <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        <p className="mt-5 mb-3 text-muted">&copy; 2017–2023</p>
       </form>
     </main>
   );
